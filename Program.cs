53141ffa390edb500bd6a39c41c4f117da34fd25
Filip Nilsson <filmarnil@gmail.com>
2021-12-04 11:18:16 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace advent_of_code_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var day1 = new Day1();
+            day1.Part2();
+        }
+    }
+}
