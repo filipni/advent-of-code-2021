522bbfef440c413196dce136bbe42241bf8790e2
Filip Nilsson <filmarnil@gmail.com>
2021-12-04 18:52:23 +0100

    Add solution for Day 3 Part 2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,1 @@
-﻿Day3.Part1();
+﻿Day3.Part2();
--- solutions/Day3.cs
+++ solutions/Day3.cs
@@ -6,13 +6,38 @@ using System.Collections.Generic;
 
 public static class Day3
 {
-    public static void Part1() {
+    public static void Part1()
+    {
         var rawInput = File.ReadAllLines("input/day3.txt");
         var codeLength = rawInput.First().Count();
         var parsedInput = rawInput.Select(x => Convert.ToInt32(x, 2)).ToList(); 
 
         var powerConsumption = CalculatePowerConsumption(parsedInput, codeLength);
         System.Console.WriteLine($"Part 1: {powerConsumption}");
+    }
+
+    public static void Part2()
+    {
+        var rawInput = File.ReadAllLines("input/day3.txt");
+        var codeLength = rawInput.First().Count();
+        var parsedInput = rawInput.Select(x => Convert.ToInt32(x, 2)).ToList(); 
+
+        var oxygenGeneratorRating = CalculateGasRating(parsedInput, codeLength, (int zeros, int ones) => zeros > ones);
+        var co2ScrubberRating = CalculateGasRating(parsedInput, codeLength, (int zeros, int ones) => zeros <= ones);
+
+        System.Console.WriteLine($"Part 2: {oxygenGeneratorRating * co2ScrubberRating}");
+    }
+
+    private static int CalculateGasRating(IEnumerable<int> diagnosticCodes, int codeLength, Func<int, int, bool> Compare)
+    {
+        if (diagnosticCodes.Count() == 1)
+            return diagnosticCodes.First();
+
+        var lookup = diagnosticCodes.ToLookup(code => (code >> codeLength - 1) & 1, code => code);
+
+        return Compare(lookup[0].Count(), lookup[1].Count())
+            ? CalculateGasRating(lookup[0], codeLength - 1, Compare)
+            : CalculateGasRating(lookup[1], codeLength - 1, Compare);
     }
 
     private static int CalculatePowerConsumption(List<int> diagnosticCodes, int codeLength)
@@ -42,6 +67,4 @@ public static class Day3
 
         return gammaRate * epsilonRate;
     }
-
-    public static void Part2() { }
 }
