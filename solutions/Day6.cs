14c99796ee6e563f04e34bfeacdd02e50fba113f
Filip Nilsson <filmarnil@gmail.com>
2022-01-17 10:22:17 +0100

    Update solution for day 6

--- solutions/Day6.cs
+++ solutions/Day6.cs
@@ -2,8 +2,6 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
-using System.Collections.Concurrent;
-using System.Threading.Tasks;
 
 static class Day6
 {
@@ -13,39 +11,49 @@ static class Day6
 
     public static void Part1()
     {
-        var numberOfFish = SimulateFish(Input, 80).Count();
+        var numberOfFish = SimulateFish(Input, 80);
         Console.WriteLine($"Part 1: {numberOfFish}"); 
     }
 
     public static void Part2()
     {
-        var numberOfFish = SimulateFish(Input, 256).Count();
+        var numberOfFish = SimulateFish(Input, 256);
         Console.WriteLine($"Part 1: {numberOfFish}"); 
     }
 
-    private static IEnumerable<int> SimulateFish(IEnumerable<int> ages, int days)
+    private static long SimulateFish(IEnumerable<int> ages, int days)
     {
+        Dictionary<int, long> ageCount = new()
+        {
+            [-1] = 0,
+            [0] = 0,
+            [1] = 0,
+            [2] = 0,
+            [3] = 0,
+            [4] = 0,
+            [5] = 0,
+            [6] = 0,
+            [7] = 0,
+            [8] = 0
+        };
+
+        foreach (var age in ages)
+            ageCount[age] += 1;
+
         while (days > 0)
         {
-            ConcurrentBag<int> updatedAges = new();
+            Dictionary<int, long> updatedAgeCount = new();
+            for (int i = 0; i <= 8; i++)
+                updatedAgeCount[i - 1] = ageCount[i];
 
-            Parallel.ForEach(ages, age =>
-            {
-                if (age == 0)
-                {
-                    updatedAges.Add(6);
-                    updatedAges.Add(8);
-                }
-                else
-                {
-                    updatedAges.Add(age - 1);
-                }
-            });
+            updatedAgeCount[8] = updatedAgeCount[-1];
+            updatedAgeCount[6] = updatedAgeCount[6] + updatedAgeCount[-1];
+            updatedAgeCount[-1] = 0;
 
-            ages = updatedAges;
+            ageCount = updatedAgeCount;
             days--;
         }
 
-        return ages;
+        return ageCount.Sum(kvp => kvp.Value);
     }
 }
